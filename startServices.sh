24714ff8930e09b2b13c7d8317199f26707b5d95
Lerk <lukas@k40s.net>
2019-12-15 18:11:55 +0100

    Update startServices.sh

--- startServices.sh
+++ startServices.sh
@@ -7,6 +7,8 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
+echo "This script is deprecated. It's a better decision to use Docker to run Postgres and Redis."
+sleep 5
 echo Detected: ${machine}
 if [ "$machine" = "Mac" ]; then
   echo "Staring PostgreSQL..."
